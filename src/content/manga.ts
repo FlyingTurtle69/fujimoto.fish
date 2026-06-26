fb58b419727f1b03cbd8f31c7f993e06df187c4c
FlyingTurtle69 <58875407+FlyingTurtle69@users.noreply.github.com>
2026-06-26 21:26:31 +1000

    Date and published state

--- src/content/manga.ts
+++ src/content/manga.ts
@@ -3,9 +3,49 @@ import type { Alias } from "./aliases";
 
 interface ReleaseDate {
   year: number;
-  month: number; // using 1 as January
-  day: number;
+  month?: number; // using 1 as January
+  day?: number;
   certainty: "=" | "<" | "?";
+}
+
+const ENG_MONTHS = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+export function displayDate({ year, month, day, certainty }: ReleaseDate, locale: Locale) {
+  let result = certainty === "<" ? "<" : "";
+  if (locale === "jp") {
+    result += `${year}年`;
+    if (month) {
+      result += `${month}月`;
+    }
+    if (day) {
+      result += `${day}日`;
+    }
+  } else {
+    if (day) {
+      result += `${day} `;
+    }
+    if (month) {
+      result += `${ENG_MONTHS[month - 1]} `;
+    }
+    result += year;
+  }
+  if (certainty === "?") {
+    result += "?";
+  }
+  return result;
 }
 
 interface MangaBase {
